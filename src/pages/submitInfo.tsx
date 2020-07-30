62cff49f17323e1bc5ff4f38f06b75f8249c9a65
kimberlyzhu <kimberly.zhu.2000@gmail.com>
2020-07-29 20:59:21 -0700

    submit info conver to server

--- src/pages/submitInfo.tsx
+++ src/pages/submitInfo.tsx
@@ -5,21 +5,12 @@ import { RouteComponentProps } from "react-router-dom";
 
 import styled from "@emotion/styled";
 import logo from "./img/meetingGroundLogo.png";
-import { Link } from "react-router-dom";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import { useQuery } from "@apollo/react-hooks";
 // yarn add react-hook-form
 //You have to use the link component to link between you pages
-import { useQuery } from "@apollo/react-hooks";
+import { useMutation } from "react-apollo";
 import gql from "graphql-tag";
-import { ApolloClient } from "apollo-client";
-import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
-import { HttpLink } from "apollo-link-http";
-import { ApolloProvider } from "react-apollo";
-import * as config from "../apiGoogleconfig.json";
-import ScheduleCard from "./ScheduleCard/component";
-import { resolve } from "url";
-import { rejects } from "assert";
-import axios from "axios";
 
 interface SubmitPagePropsInterface
   extends RouteComponentProps<{ id: string; time: string }> {
@@ -77,39 +68,57 @@ const urlId: {
 } = {
   urlid: "",
 };
-
-async function getNewAccessToken(refreshToken: any) {
-  let data = {
-    client_id: config.config.clientId,
-    client_secret: config.config.clientSecret,
-    refresh_token: refreshToken,
-    grant_type: "refresh_token",
-  };
-  let myaccesstoken = await fetch(
-    "https://www.googleapis.com/oauth2/v2/token",
-    {
-      method: "post",
-      headers: {
-        Content_Type: "application/json",
-        // client_id: config.config.clientId,
-        // client_secret: config.config.clientSecret,
-        // refresh_token: refreshToken,
-        // grant_type: "refresh_token",
-      },
-      body: JSON.stringify(data),
-      // body: {
-      //   client_id: config.config.clientId,
-      //   client_secret: config.config.clientSecret,
-      //   refresh_token: refreshToken,
-      //   grant_type: "refresh_token",
-      // }
+const createEvent = gql`
+  mutation(
+    $url: String!
+    $email: String!
+    $username: String!
+    $comment: String!
+    $startTime: String!
+  ) {
+    create_event(
+      url: $url
+      email: $email
+      username: $username
+      comment: $comment
+      startTime: $startTime
+    ) {
+      state
     }
-  )
-    .then((response) => {
-      console.log(response.json());
-    })
-    .then((json) => console.log(json));
-}
+  }
+`;
+// async function getNewAccessToken(refreshToken: any) {
+//   let data = {
+//     client_id: config.config.clientId,
+//     client_secret: config.config.clientSecret,
+//     refresh_token: refreshToken,
+//     grant_type: "refresh_token",
+//   };
+//   let myaccesstoken = await fetch(
+//     "https://www.googleapis.com/oauth2/v2/token",
+//     {
+//       method: "post",
+//       headers: {
+//         Content_Type: "application/json",
+//         // client_id: config.config.clientId,
+//         // client_secret: config.config.clientSecret,
+//         // refresh_token: refreshToken,
+//         // grant_type: "refresh_token",
+//       },
+//       body: JSON.stringify(data),
+//       // body: {
+//       //   client_id: config.config.clientId,
+//       //   client_secret: config.config.clientSecret,
+//       //   refresh_token: refreshToken,
+//       //   grant_type: "refresh_token",
+//       // }
+//     }
+//   )
+//     .then((response) => {
+//       console.log(response.json());
+//     })
+//     .then((json) => console.log(json));
+// }
 
 const SubmitInfoPage: React.FC<SubmitPagePropsInterface> = (
   props: SubmitPagePropsInterface
@@ -117,6 +126,8 @@ const SubmitInfoPage: React.FC<SubmitPagePropsInterface> = (
   const id = props.match.params.id; //link id
   const scheduledDate = new Date(props.match.params.time.replace("%20", ""));
   urlId.urlid = id;
+
+  const [mutate] = useMutation(createEvent);
 
   const { register, handleSubmit } = useForm({
     defaultValues: {
@@ -127,57 +138,25 @@ const SubmitInfoPage: React.FC<SubmitPagePropsInterface> = (
     },
   });
 
-  const cache = new InMemoryCache();
-  const link = new HttpLink({
-    uri: "http://localhost:4000/graphql",
-  });
-
-  const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-    cache,
-    link,
-  });
-
   const clientEmail: {
     email: string;
   } = {
     email: "",
   };
 
-  const requestSlot = useRequestSlot();
-  // // requestSlot() is a function that you call to make the request.
-  // // Initially, you can dummy it out like this:
-  function useRequestSlot(): (
-    time: Date,
-    linkUrl: string,
-    guestFirst: string,
-    guestLast: string
-  ) => Promise<boolean> {
-    // return () => Promise.resolve();
-    return (
-      time: Date,
-      linkUrl: string,
-      guestFirst: string,
-      guestLast: string
-    ) =>
-      new Promise<boolean>(function (resolve, reject) {
-        initiate()
-          .then(() => resolve(true))
-          .catch(() => reject(new Error("useRequestSlot")));
-      });
+  async function inviteTo(data: any) {
+    let myComment = data.comment !== undefined ? data.comment : " ";
+    const arg = await mutate({
+      variables: {
+        url: urlId.urlid,
+        email: data.email,
+        username: data.firstName + " " + data.lastName,
+        comment: myComment,
+        startTime: scheduledDate.toString(),
+      },
+    });
   }
-  //when the backend is ready, you can change it to something like:
-  // function useRequestSlot(): (time: number) => Promise<boolean> {
-  //     const mutate = useMutation(RequestSlot);
-  //     return (time:number) => mutate({variables: { time } });  // }
 
-  // TODO insert accesstoken string here
-  let accessToken: string =
-    "ya29.a0AfH6SMDY4D_R4A1nSFVCj8-K0KHg48bo9Y2ooPX07QDFebx4VOrrWInQeCUlz8RFtI-EkycMcNNxMlQ6IjIAXitlP7r_3C24l8yMDOGnGUPuL5qNCjfXG0ShnpyTZmj6Y02shxXEiSrc-NFub5GWOH24pQ0IJm1Z6BQ";
-
-  let refreshToken: string =
-    "4/2QHycMpj5E3KlyDm5aRjiz_5u3UqSN5rQwEbherjrsgyfvmtNJ2QUt9PDrhheBHIWgZdhd0HaqibUR5HSU76vWQ";
-  //getNewAccessToken(refreshToken);
-  getNewAccessToken(refreshToken);
   const onSubmit = (data: any) => {
     console.log(data);
     console.log(data.firstName);
@@ -188,205 +167,10 @@ const SubmitInfoPage: React.FC<SubmitPagePropsInterface> = (
     ) {
       window.alert("invalid email");
     } else {
-      //setReadyToSend(true);
-      //initiate();
-      requestSlot(scheduledDate, urlId.urlid, data.firstName, data.lastName)
-        .then((result) => {
-          if (result) {
-            console.log("scheduled time succesfully:" + result);
-            props.history.push("/confirmation");
-          }
-          //  else {
-          //   console.log("could not schedule event");
-          //   props.history.push("/signup/" + urlId.urlid);
-          // }
-        })
-        .catch((err) => {
-          console.log("could not schedule event");
-          props.history.push("/signup/" + urlId.urlid);
-        });
+      inviteTo(data);
+      // Submit(data);
     }
   };
-
-  function ISODateString(d: Date) {
-    function pad(n: number) {
-      return n < 10 ? "0" + n : n;
-    }
-    return (
-      d.getFullYear() +
-      "-" +
-      pad(d.getMonth() + 1) +
-      "-" +
-      pad(d.getDate()) +
-      "T" +
-      pad(d.getHours()) +
-      ":" +
-      pad(d.getMinutes()) +
-      ":" +
-      pad(d.getSeconds()) +
-      "-" +
-      pad(d.getTimezoneOffset() / 60) +
-      ":00"
-    );
-  }
-
-  // ________________________________________________________________________________________
-  function initiate(): Promise<void> {
-    return new Promise((resolve, reject) => {
-      gapi.load("client:auth2", () => {
-        initClient()
-          .then(() => {
-            resolve();
-          })
-          .catch((err) => {
-            reject(new Error("initiate doesn't work"));
-          });
-      });
-    });
-  }
-
-  function initClient(): Promise<void> {
-    // const values = {
-    //   'client_id': config.config.clientId,
-    //   'scope': config.config.scope,
-    // }
-    // gapi.auth2.authorize(values, function() {
-    //   console.log('login complete');
-    //   console.log(gapi.auth.getToken());
-    // });
-    //
-    let eventSuccess: any;
-    //return new Promise(function (resolve, reject) {
-    return new Promise((resolve, reject) => {
-      gapi.client
-        .init({
-          apiKey: config.config.apiKey,
-          clientId: config.config.clientId,
-          discoveryDocs: config.config.discoveryDocs,
-          scope: config.config.scope,
-        })
-        .then(
-          () => {
-            // Listen for sign-in state changes.
-            // gapi.auth2.authorize;
-
-            // gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-            // updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-            gapi.client.setToken({
-              access_token: accessToken,
-              //refresh_token: accessToken,
-            });
-          },
-          function (error: any) {
-            console.log("An errot in update");
-            //appendPre(JSON.stringify(error, null, 2));
-          }
-        )
-        .then(() => {
-          //returns a promise
-          accessUserOffline()
-            .then(() => {
-              console.log("success in initiate");
-              resolve();
-            })
-            .catch((err) => {
-              console.log(err);
-              reject(new Error("accessuseroffline doesn't work"));
-            });
-        });
-    });
-
-    //resolve({ eventStatus: eventSuccess });
-    //});
-  }
-  function updateSigninStatus(isSignedIn: boolean) {
-    if (isSignedIn) {
-      console.log("SIGNED IN");
-    } else {
-      console.log("SIGNED OUT");
-      // getUsersCalendarList(
-      //   "ya29.a0AfH6SMDsk-PBxuG0etR5n4CdEN7K8JFCam6zIA4vEEcZ71qYENWBV8SuP1XoktDhmhTlCMhrteGWUchJExi6_oO-6c2RbWylklmwUvDZM2EwpSNl1iALYgMMxKA0u0_7KQ1AFxprea4RUiTlOseqAjIPGA6H7gd0FQM"
-      // );
-    }
-
-    accessUserOffline();
-  }
-
-  // *****************************'
-  async function getUsersCalendarList(accessToken: any) {
-    let calendarsList = await fetch(
-      "https://www.googleapis.com/auth/calendar.events",
-      {
-        method: "get",
-        headers: { Authorization: `Bearer ${accessToken}` },
-      }
-    )
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-    // return calendarsList.json();
-    // console.log(calendarsList.json());
-  }
-
-  // ________________________________________________________________________________________
-  const endTimeSchedule: Date = new Date(
-    scheduledDate.getTime() + 60 * 1000 * meetingInfo.duration
-  );
-  const start: string = scheduledDate.toString();
-  const end: string = endTimeSchedule.toString();
-  const timeZone: string = scheduledDate.getTimezoneOffset().toString();
-  console.log(start);
-  console.log(end);
-  console.log(timeZone);
-
-  function accessUserOffline() {
-    const event = {
-      summary: "Meeting with " + HostInfo.firstname + " " + HostInfo.lastname,
-      location: "Online",
-      description: "You have been invited to this Meeting",
-      start: {
-        // date: scheduledDate.toISOString(),
-        dateTime: ISODateString(scheduledDate),
-        timeZone: "Etc/UTC",
-      },
-      end: {
-        // date: scheduledDate.toISOString(),
-        dateTime: ISODateString(
-          new Date(scheduledDate.getTime() + 60 * 1000 * meetingInfo.duration)
-        ),
-        timeZone: "Etc/UTC",
-      },
-      recurrence: ["RRULE:FREQ=DAILY;COUNT=1"],
-      attendees: [{ email: clientEmail.email }],
-      visibility: "default",
-      reminders: {
-        useDefault: false,
-        overrides: [
-          { method: "email", minutes: 24 * 60 },
-          { method: "popup", minutes: 10 },
-        ],
-      },
-    };
-    let eventScheduledHere = false;
-
-    return new Promise((resolve, reject) => {
-      gapi.client.calendar.events
-        .insert({
-          calendarId: "primary",
-          resource: event,
-          sendNotifications: true,
-          sendUpdates: "all",
-        })
-        .execute((event) => {
-          console.log("event status:" + event.status);
-
-          if (event.status?.toLocaleString() === "confirmed") {
-            resolve({ eventScheduledHere: true });
-          } else {
-            reject(new Error("event not submmitted"));
-          }
-        });
-    });
-  }
 
   return (
     <body style={{ background: "rgba(131, 196, 197)" }}>
@@ -473,85 +257,84 @@ const SubmitInfoPage: React.FC<SubmitPagePropsInterface> = (
               </button>
             </div>
           </form>
-          d
         </MainBodyFormat>
-        {true && (
-          <ApolloProvider client={client}>
-            <GetHostEmail />
-          </ApolloProvider>
-        )}
+        {/* {true && (
+          // <ApolloProvider client={client}>
+          // <GetHostEmail />
+          // </ApolloProvider>
+        )} */}
         <pre id="content"></pre>
       </div>
     </body>
   );
 };
 
-const HostInfo: {
-  firstname: string;
-  lastname: string;
-  code: string;
-} = {
-  code: "",
-  firstname: "",
-  lastname: "",
-};
+// const HostInfo: {
+//   firstname: string;
+//   lastname: string;
+//   code: string;
+// } = {
+//   code: "",
+//   firstname: "",
+//   lastname: "",
+// };
 
-function GetHostEmail() {
-  const [tempdata, setdata] = useState<any>();
-  const {
-    loading: loadingEmail,
-    error: errorEmail,
-    data: dataEmail,
-  } = useQuery(GET_HOST_EMAIL, {
-    variables: { id: urlId.urlid },
-  });
+// function GetHostEmail() {
+//   const [tempdata, setdata] = useState<any>();
+//   const {
+//     loading: loadingEmail,
+//     error: errorEmail,
+//     data: dataEmail,
+//   } = useQuery(GET_HOST_EMAIL, {
+//     variables: { id: urlId.urlid },
+//   });
 
-  loadingEmail
-    ? console.log("loading Email")
-    : errorEmail
-    ? console.log("An Error occurred:" + { errorEmail })
-    : (meetingInfo.hostEmail = dataEmail.link.email) &&
-      (meetingInfo.duration = dataEmail.link.duration);
+//   loadingEmail
+//     ? console.log("loading Email")
+//     : errorEmail
+//     ? console.log("An Error occurred:" + { errorEmail })
+//     : (meetingInfo.hostEmail = dataEmail.link.email) &&
+//       (meetingInfo.duration = dataEmail.link.duration);
 
-  console.log(meetingInfo.hostEmail);
+//   console.log(meetingInfo.hostEmail);
 
-  const { loading: loadingCode, error: errorCode, data: dataCode } = useQuery(
-    GET_GOA_CODE,
-    {
-      variables: { id: meetingInfo.hostEmail },
-    }
-  );
+//   const { loading: loadingCode, error: errorCode, data: dataCode } = useQuery(
+//     GET_GOA_CODE,
+//     {
+//       variables: { id: meetingInfo.hostEmail },
+//     }
+//   );
 
-  loadingCode
-    ? console.log("loading access")
-    : errorCode
-    ? console.log("An Error occurred:" + { errorCode })
-    : (HostInfo.code = dataCode.email.goa_code) &&
-      (HostInfo.firstname = dataCode.email.firstname) &&
-      (HostInfo.lastname = dataCode.email.lastname);
+//   loadingCode
+//     ? console.log("loading access")
+//     : errorCode
+//     ? console.log("An Error occurred:" + { errorCode })
+//     : (HostInfo.code = dataCode.email.goa_code) &&
+//       (HostInfo.firstname = dataCode.email.firstname) &&
+//       (HostInfo.lastname = dataCode.email.lastname);
 
-  console.log(HostInfo.code);
+//   console.log(HostInfo.code);
 
-  return <div>the Email is authentified</div>;
-}
+//   return <div>the Email is authentified</div>;
+// }
 
-const GET_HOST_EMAIL = gql`
-  query($id: String) {
-    link(id: $id) {
-      email
-      duration
-    }
-  }
-`;
+// const GET_HOST_EMAIL = gql`
+//   query($id: String) {
+//     link(id: $id) {
+//       email
+//       duration
+//     }
+//   }
+// `;
 
-const GET_GOA_CODE = gql`
-  query($id: String) {
-    email(id: $id) {
-      goa_code
-      firstname
-      lastname
-    }
-  }
-`;
+// const GET_GOA_CODE = gql`
+//   query($id: String) {
+//     email(id: $id) {
+//       goa_code
+//       firstname
+//       lastname
+//     }
+//   }
+// `;
 
 export default SubmitInfoPage;
